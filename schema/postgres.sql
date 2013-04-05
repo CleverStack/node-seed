37cfb65de35d60f680f70c2e42763d257f928bdc
Richard Gustin <richard.gustin86@gmail.com>
2013-04-06 01:51:31 +1000

    Fixing up contrib

--- schema/postgres.sql
+++ schema/postgres.sql
@@ -1,3 +1,19 @@
+---------------------------------------------------------------------
+-- CONTRIB SCHEMA
+-- This is not mandatory, but it's traditional to put all 3rd
+-- party extensions in a contrib schema. Notably pgcrypto for
+-- password hashing
+
+drop schema if exists contrib cascade;
+create schema contrib authorization postgres;
+grant all on schema contrib to public;
+
+
+-- Install pgcrypto for password encryption
+
+create extension pgcrypto schema contrib;
+
+---------------------------------------------------------------------
 create or replace function public.generate_credentials() 
 returns trigger as $$
 	begin
