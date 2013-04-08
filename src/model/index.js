c5df6c949da5a259a6d4ecb7d6c5e14a6e1063de
Richard Gustin <richard.gustin86@gmail.com>
2013-04-08 21:45:50 +1000

    New way of defining models and their relationships

--- src/model/index.js
+++ src/model/index.js
@@ -1,20 +1,33 @@
-module.exports = function(sequelize) {
+module.exports = function(sequelize, config) {
 	var exports = m = {};
-	var models = [
-		// Define the names of your models here
-		'User',
-		'Role'
-	]
 
 	// Add them to exports
-	models.forEach(function(model, i) {
+	config.models.forEach(function(model, i) {
 		m[model] = sequelize.import(__dirname + '/' + model);
 	})
 
 	// Define relationships
-	
-	m.User.hasMany(m.Role);
-	m.Role.hasMany(m.User);
+	config.models.forEach(function(modelName) {
+		if (config.modelAssociations[modelName] != undefined) {
+			Object.keys(config.modelAssociations[modelName]).forEach(function(assocType) {
+				var associatedWith = config.modelAssociations[modelName][assocType];
+				if (!associatedWith instanceof Array) {
+					associatedWith = [associatedWith];
+				}
+
+				associatedWith.forEach(function(assocTo) {
+					// Support second argument
+					if (assocTo instanceof Array) {
+						// console.log([modelName, assocType, assocTo[1]].join(' '));
+						m[modelName][assocType](m[assocTo[0]], assocTo[1]);
+					} else {
+						// console.log([modelName, assocType, assocTo].join(' '));
+						m[modelName][assocType](m[assocTo]);
+					}
+				})
+			})
+		}
+	});
 
 	return m;
 }
