1eaa8538a2b2e01a143f70b89a0bc7fe40181c53
Richard Gustin <richard.gustin86@gmail.com>
2013-06-22 17:14:43 +1000

    Changed over to use the "clever-controller" module

--- src/classes/Controller.js
+++ src/classes/Controller.js
@@ -1,181 +1,53 @@
 var NoActionException = require('./../exceptions/NoAction'),
-    Class = require('uberclass');
+    Controller = require('clever-controller');
 
-module.exports = Class.extend(
+module.exports = Controller.extend(
 /* @Static */
 {
-    actionsEnabled: true,
-
-    httpMethodsEnabled: true,
-
-    attach: function() {
-        return this.callback('newInstance');
-    },
-
     service: null
 },
 /* @Prototype */
 {
-    req: null,
-    res: null,
-    next: null,
-    resFunc: 'json',
-    action: null,
-
-    setup: function(req, res, next) {
-        try {
-            return this.performanceSafeSetup(req, res, next);
-        } catch(e) {
-            return [e];
-        }
-    },
-
-    performanceSafeSetup: function(req, res, next) {
-        var method = null,
-            funcName = null;
-
-        this.next = next;
-        this.req = req;
-        this.res = res;
-
-        // Override routes where you attach specifically to a single route
-        if (this.Class.actionsEnabled && /\//.test(this.req.url)) {
-            funcName = this.req.url.split('/')[2];
-            if (isNaN(funcName)) {
-                funcName = funcName + 'Action';
-                if (typeof this[funcName] == 'function') {
-                    return [null, funcName, next];
-                }
-            }
-        }
-
-        // Route based on an action first if we can
-        if (this.Class.actionsEnabled && typeof this.req.params.action != 'undefined') {
-            // Action Defined Routing
-            if (isNaN(this.req.params.action)) {
-                funcName = this.req.params.action + 'Action';
-
-                if (typeof this[funcName] == 'function') {
-                    return [null, funcName, next];
-                } else {
-                    throw new NoActionException();
-                }
-            } else {
-                // HTTP Method Based Routing
-                method = this.req.method.toLowerCase() + 'Action';
-                if (typeof this[method] == 'function') {
-
-                    this.req.params.id = this.req.params.action;
-                    delete this.req.params.action;
-
-                    return [null, method, next];
-                } else {
-                    throw new NoActionException();
-                }
-            }
-        }
-
-        // Route based on the HTTP Method, otherwise throw an exception
-        if (this.Class.httpMethodsEnabled) {
-            if (this.isGet() && (this.req.params === undefined || this.req.params.id === undefined) && typeof this.listAction == 'function') {
-                method = 'listAction';
-            } else {
-                method = this.req.method.toLowerCase() + 'Action';
-                if (typeof this[method] != 'function') {
-                    throw new NoActionException();
-                }
-            }
-        }
-
-        // If we got this far without an action but with a method, then route based on that
-        return [null, method, next];
-    },
-
-    init: function(error, method, next) {
-        if (error && error instanceof NoActionException) {
-            this.next();
-        } else {
-            try {
-                if (error)
-                    throw error;
-
-                if (method !== null) {
-                    this.action = method;
-                    this[method](this.req, this.res);
-                } else {
-                    this.next();
-                }
-
-            } catch(e) {
-                this.handleException(e);
-            }
-        }
-    },
-
-    send: function(content, code, type) {
-        var toCall = type || this.resFunc;
-        if (code) {
-            this.res[toCall](code, content);
-        } else {
-            this.res[toCall](content);
-        }
-    },
-
-    render: function(template, data) {
-        this.res.render(template, data);
-    },
-
-    handleException: function(exception) {
-        this.send({ error: 'Unhandled exception: ' + exception, stack: exception.stack }, 500);
-    },
-
-    isGet: function() {
-        return this.req.method.toLowerCase() == 'get';
-    },
-
-    isPost: function() {
-        return this.req.method.toLowerCase() == 'post';
-    },
-
-    isPut: function() {
-        return this.req.method.toLowerCase() == 'put';
-    },
-
     listAction: function() {
-        if (this.Class.service !== null) {
-            this.Class.service.findAll()
-                .then(this.proxy('send'))
-                .fail(this.proxy('handleException'));
+        if ( this.Class.service !== null ) {
+            var query = this.req.query;
+            var options = {};
+            if ( Object.keys( query ).length ) {
+                options.where = query;
+            }
+            this.Class.service.find( options )
+                .then( this.proxy( 'send' ) )
+                .fail( this.proxy( 'handleException' ) );
         } else {
             this.next();
         }
     },
 
     getAction: function() {
-        if (this.Class.service !== null) {
-            this.Class.service.findById(this.req.params.id)
-                .then(this.proxy('send'))
-                .fail(this.proxy('handleException'));
+        if ( this.Class.service !== null ) {
+            this.Class.service.findById( this.req.params.id )
+                .then( this.proxy( 'send' ) )
+                .fail( this.proxy( 'handleException' ) );
         } else {
             this.next();
         }
     },
 
     postAction: function() {
-        if (this.Class.service !== null) {
-            this.Class.service.create(this.req.body)
-                .then(this.proxy('send'))
-                .fail(this.proxy('handleException'));
+        if ( this.Class.service !== null ) {
+            this.Class.service.create( this.req.body )
+                .then( this.proxy( 'send' ) )
+                .fail( this.proxy( 'handleException' ) );
         } else {
             this.next();
         }
     },
 
     putAction: function() {
-        if (this.Class.service !== null) {
-            this.Class.service.update(this.req.params.id, this.req.body)
-                .then(this.proxy('send'))
-                .fail(this.proxy('handleException'));
+        if ( this.Class.service !== null ) {
+            this.Class.service.update( this.req.params.id, this.req.body )
+                .then( this.proxy( 'send' ) )
+                .fail( this.proxy( 'handleException' ) );
         } else {
             this.next();
         }
@@ -183,9 +55,9 @@ module.exports = Class.extend(
 
     deleteAction: function() {
         if (this.Class.service !== null) {
-            this.Class.service.destroy(this.req.params.id)
-                .then(this.proxy('send'))
-                .fail(this.proxy('handleException'));
+            this.Class.service.destroy( this.req.params.id )
+                .then( this.proxy( 'send' ) )
+                .fail( this.proxy( 'handleException' ) );
         } else {
             this.next();
         }
