2f81d947cae4e266311b911ec12d405cac111b6d
Daniel Young <young.daniel88@gmail.com>
2013-08-11 15:44:07 +1000

    CORS;

--- index.js
+++ index.js
@@ -51,6 +51,22 @@ app.configure( function() {
         })
     }));
 
+    // Enable CORS
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", req.headers.origin);
+      res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+      res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+      res.header("Access-Control-Allow-Credentials", "true");
+      res.header("Access-Control-Max-Age", "1000000000");
+      // intercept OPTIONS method
+      if ('OPTIONS' == req.method) {
+        res.send(200);
+      }
+      else {
+        next();
+      }
+    });
+
     app.use( express.logger('dev') );
     app.use( express.compress() );
     app.use( express.favicon() );
