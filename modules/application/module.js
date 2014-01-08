df078813b017375bb1cd45d0413d89f6c5507b4f
Richard Gustin <richard.gustin86@gmail.com>
2014-01-09 02:43:55 +1000

    Indent using spaces and added missing BaseService

--- modules/application/module.js
+++ modules/application/module.js
@@ -1,5 +1,5 @@
 module.exports = ( require( './classes/ModuleClass' ) ).extend({
-	init: function() {
-		console.log( 'inside the extending class for the application module' );
-	}
+    init: function() {
+        console.log( 'inside the extending class for the application module' );
+    }
 });
