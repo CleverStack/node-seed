7bee6e0e7a3feceeb68f6ef75744bac0e90b609d
Richard Gustin <richard.gustin86@gmail.com>
2014-01-10 09:45:02 +1000

    Removing old files left over from the old structure

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-var m = {};
-require('fs').readdirSync(__dirname + '/').forEach(function(file) {
-    if (file.match(/.+\.js/g) !== null && file !== 'index.js') {
-        var name = file.replace('.js', '');
-        m[name] = require('./' + file);
-    }
-});    
-module.exports = m;
--- src/utils/stacktrace.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function () {
-    return new Error().stack.split('\n').splice(2);
-};
