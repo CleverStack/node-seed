4c35c6a92409769787b9f106b48807039ea4cffb
Richard Gustin <richard.gustin86@gmail.com>
2014-01-11 01:56:54 +1000

    Fixed the ORM module loading and the example-module model

--- modules/example-module/routes.js
+++ modules/example-module/routes.js
@@ -1,4 +1,4 @@
-module.exports = function( app, ExampleController ) {
+module.exports = function( app, ExampleController, TestModel ) {
     // Define routes here
     app.all('/example/:action/:id?', ExampleController.attach());
     app.all('/example/?:action?', ExampleController.attach());
--- modules/orm/module.js
+++ modules/orm/module.js
@@ -1,12 +1,14 @@
 var Sequelize = require( 'sequelize' )
-  , modelInjector = require( 'utils' ).modelInjector
-  , sequelize
   , debug = require( 'debug' )( 'ORM' )
   , config = injector.getInstance( 'config' )
   , ModuleClass = require( 'classes' ).ModuleClass
+  , sequelize
   , Module;
 
 Module = ModuleClass.extend({
+
+    loadedModels: null,
+
     setup: function() {
         debug( 'Opening connection to database' );
 
@@ -20,26 +22,22 @@ Module = ModuleClass.extend({
     
     initInjector: function() {
         injector.instance( 'Sequelize', Sequelize );
+        injector.instance( 'DataTypes', Sequelize );
         injector.instance( 'sequelize', sequelize );
-
-        // @TODO This is the old style do we still need this?
         injector.instance( 'db', sequelize );
     },
 
-    preRoute: function() {
-        // Load the models
-        var models = require( 'models' );
-
-        // Run the model injector
-        modelInjector( models );
-
-        // Add it to our injector instace
-        injector.instance( 'models', models );
-    },
-
     loadModel: function( modelPath ) {
-        debug( 'Loading model from ' + modelPath );
-        return sequelize.import( modelPath );
+        var modelName = 'ORM' + modelPath.split( '/' ).pop()
+          , model = injector.getInstance( modelName );
+
+        if ( !model ) {
+            debug( 'Loading model from ' + modelPath );
+            model = sequelize.import( modelPath );
+            injector.instance( modelName, model );
+        }
+
+        return model;
     }
 });
 
