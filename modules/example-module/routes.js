2cec91975f7a296deb9daa984634fbe328bf98e1
Richard Gustin <richard.gustin86@gmail.com>
2014-01-11 03:48:03 +1000

    Loading example ODM model inside routes

--- modules/example-module/routes.js
+++ modules/example-module/routes.js
@@ -1,4 +1,4 @@
-module.exports = function( app, ExampleController, ORMExampleModel ) {
+module.exports = function( app, ExampleController, ORMExampleModel, ODMExampleModel ) {
     // Define routes here
     app.all('/example/:action/:id?', ExampleController.attach());
     app.all('/example/?:action?', ExampleController.attach());
