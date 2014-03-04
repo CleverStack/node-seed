a45ad2191c75e3cf0aac30610ded17463a70912c
pilsy <richard.gustin86@gmail.com>
2014-03-04 14:22:16 +1000

    feat(module-hooks): Implemented the ability for each module to define their own bin/app.js file that can be executed when the main app.js file is executed

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var cluster = require( 'cluster' )
-  , cp = require( 'child_process' )
   , config = require( './config' )
   , packageJson = require( './package.json' )
-  , backgroundTasks = null
+  , path = require( 'path' )
+  , fs = require( 'fs' );
+
+function loadModulesAppFiles( moduleName ) {
+    packageJson.bundledDependencies.forEach(function( moduleName ) {
+        var file = [ path.resolve( './' ), 'modules', moduleName, 'bin', 'app.js' ].join( path.sep );
+        if ( fs.existsSync( file ) ) {
+            require( file )( cluster, config, packageJson );
+        }
+    });
+}
 
 // Set the node path - this works only because the other processes are forked.
 process.env.NODE_PATH = process.env.NODE_PATH ? './lib/:./modules/:' + process.env.NODE_PATH : './lib/:./modules/';
@@ -12,39 +21,17 @@ if ( cluster.isMaster ) {
         console.dir( arguments );
         cluster.fork();
     });
+
     for ( var i=0; i<config.numChildren; ++i ) {
         cluster.fork();
     }
 
-    Object.keys( cluster.workers ).forEach(function( id ) {
-        cluster.workers[ id ].on('message', function( msg ) {
-            console.log('Master ' + process.pid + ' received message from worker ' + id + '.', msg);
-            
-            //Send message to background task
-            if( msg.cmd == 'backgroundTask' && config[ 'background-tasks' ] && config[ 'background-tasks' ].on === true){
-                backgroundTasks.send({ cmd: 'master', task:msg.task, wrkid: id });
-            }
-        });
-    });
-
-    if ( packageJson.bundledDependencies.indexOf( 'background-tasks' ) !== -1 ) {
-        if ( config[ 'background-tasks' ] && config[ 'background-tasks' ].on === true ) {
-            function setupBackgroundTasks() {
-                console.log('Setup background tasks...');
-
-                backgroundTasks = cp.fork('./modules/background-tasks/bin/backgroundTasks.js');
-                backgroundTasks.on('exit', setupBackgroundTasks);
-                backgroundTasks.on('message', function( msg ){
-                    console.log('\nMaster ' + process.pid + ' received message from Background Task Process ' + this.pid + '.', msg);
-                    msg['cmd'] = 'master';
-                    
-                    ( !msg.wrkid ) ? backgroundTasks.send( msg ) : cluster.workers[ msg.wrkid ].send( msg ) ;
-                });
-            }
-            setupBackgroundTasks();
-        }
-    }
-
+    // moduleName/bin/app.js hook
+    loadModulesAppFiles();
 } else {
+
+    // moduleName/bin/app.js hook
+    loadModulesAppFiles();
+
     require('./index.js');
 }
