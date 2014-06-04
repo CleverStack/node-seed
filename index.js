742b5031f511331669892730d0315b5eee9a3a52
Richard Gustin <richard.gustin86@gmail.com>
2014-06-04 20:02:12 +1000

    Reworked module loader, class and hooks

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ env.app.configure(function() {
     env.app.use( env.express.compress() );
     env.app.use( env.express.favicon() );
     env.app.use( env.express.methodOverride() );
+    
+    // Allow cross-origin requests
+    env.app.use( cors( env.config.cors ) );
 });
 
 // Load all the modules
@@ -23,17 +26,14 @@ env.moduleLoader.initializeRoutes( injector );
 // Add middleware that needs to come after routes
 env.app.configure(function() {
 
-    // Allow cross-origin requests
-    env.app.use( cors( env.config.cors ) );
-
     // Attach our router
     env.app.use( env.app.router );
 
     // error handler, outputs json since that's usually
     // what comes out of this thing
     env.app.use(function( err, req, res, next ) {
-        console.log('Express error catch', err);
-        res.json(500, {
+        console.log( 'Express error catch', err );
+        res.json( 500, {
             error: err.toString()
         });
     });
@@ -41,7 +41,7 @@ env.app.configure(function() {
 
 // Listen for requests
 env.app.listen( env.webPort, function() {
-    console.log("Starting server on port " + env.webPort + " in " + env.config.environmentName + " mode");
+    console.log( "Starting server on port " + env.webPort + " in " + env.config.environmentName + " mode" );
 });
 
 // Export the Express app
