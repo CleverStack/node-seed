597b57a38387c171ec6613b357662628368cdc7c
Richard Gustin <richard.gustin86@gmail.com>
2014-06-13 19:31:08 +1000

    refactor(tests): Travis before script was not rebasing the db correctly

--- tests/before_travis_test.js
+++ tests/before_travis_test.js
@@ -115,9 +115,27 @@ function bundled() {
     return deferred.promise;    
 }
 
+function rebase() {
+    var deferred = Q.defer()
+      , proc = spawn( 'grunt', [ 'db' ], { cwd: path.resolve( path.join( __dirname, '..' ) ) } );
+
+    proc.stderr.on('data', function (data) {
+        console.log( 'Error in step #4 - ' + data.toString() + '\n');
+        defered.reject ( data.toString() );
+    });
+
+    proc.on('close', function (code) {
+        console.log('step #4 process exited with code ' + code + '\n' );
+        defered.resolve();
+    });
+
+    return deferred.promise;
+}
+
 installORM()
-    .then ( configFiles )
-    .then ( bundled )
-    .fail ( function (err) {
+    .then( configFiles )
+    .then( bundled )
+    .then( rebase )
+    .fail( function (err) {
         console.log('Error - ' + err );
     });
