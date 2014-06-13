138e30b5e801b7fc3cb34a6ff2f45f1a430fceb4
Richard Gustin <richard.gustin86@gmail.com>
2014-06-13 22:30:43 +1000

    fix(tests): Changing travis again to try and get tests running properly

--- tests/before_travis_test.js
+++ tests/before_travis_test.js
@@ -9,7 +9,7 @@ function installORM () {
     var defered = Q.defer()
       , proc = spawn ( 'clever', [ 'install', 'clever-orm' ], { cwd: path.resolve( path.join( __dirname, '..' ) ) } );
 
-    console.log( 'step #1 - install clever-orm module - begin\n' );
+    console.log( 'step #2 - install clever-orm module - begin\n' );
 
     proc.stdout.on('data', function (data) {
         var str = data.toString()
@@ -34,12 +34,12 @@ function installORM () {
     });
 
     proc.stderr.on('data', function (data) {
-        console.log( 'Error in step #1 - ' + data.toString() + '\n');
+        console.log( 'Error in step #2 - ' + data.toString() + '\n');
         defered.reject ( data.toString() );
     });
 
     proc.on('close', function (code) {
-        console.log('step #1 process exited with code ' + code + '\n' );
+        console.log('step #2 process exited with code ' + code + '\n' );
         defered.resolve();
     });
 
@@ -67,49 +67,16 @@ function configFiles() {
             }
         };
 
-    console.log( 'step #2 - create and update config files - begin\n' );
+    console.log( 'step #1 - create and update config files - begin\n' );
     fs.writeFile ( comFile, JSON.stringify( comData, null, '  ' ), function ( err ) {
 
         if ( err ) {
-            console.log( 'Error in step #2 - ' + err + '\n');
+            console.log( 'Error in step #1 - ' + err + '\n');
             return deferred.reject ( err );
         }
 
-        console.log('step #2 process exited with code 0\n' );
+        console.log('step #1 process exited with code 0\n' );
         deferred.resolve();
-    });
-
-    return deferred.promise;    
-}
-
-//added clever-orm module in bundledDependencies
-function bundled() {
-    var deferred = Q.defer()
-      , file = path.join( __dirname, '..', 'package.json' );
-
-    console.log( 'step #3 - added clever-orm module in bundledDependencies\n' );
-
-    fs.readFile ( file, function ( err, data ) {
-
-        if ( err ) {
-            console.log( 'Error in step #3 - ' + err + '\n');
-            return deferred.reject ( err );
-        }
-
-        data = JSON.parse ( data );
-
-        data.bundledDependencies.push ( 'clever-orm' );
-
-        fs.writeFile ( file, JSON.stringify ( data ), function ( err ) {
-
-            if ( err ) {
-                console.log( 'Error in step #3 - ' + err + '\n');
-                return deferred.reject ( err );
-            }
-
-            console.log('step #3 process exited with code 0\n' );
-            deferred.resolve();
-        });
     });
 
     return deferred.promise;    
@@ -117,10 +84,6 @@ function bundled() {
 
 configFiles()
     .then( installORM )
-    // .then( bundled )
-    .then( function() {
-        console.log( 'Installed ORM module' );
-    })
     .fail( function (err) {
         console.log('Error - ' + err );
     });
