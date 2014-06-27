6ecf96c074803ef58a7b13b29e90ee64a5c85818
Richard Gustin <richard.gustin86@gmail.com>
2014-06-27 15:48:27 +1000

    fix(tests): Stacktrace tests have been updated to work with chai

--- tests/unit/test.utils.stacktrace.js
+++ tests/unit/test.utils.stacktrace.js
@@ -2,34 +2,33 @@
 
 // Bootstrap the testing environmen
 var stacktrace = require('utils').stacktrace
-  , should = require( 'should' );
+  , expect     = require( 'chai' ).expect;
 
-describe('utils', function () {
-    describe('.stacktrace()', function () {
-        it('should return array', function () {
-            var stack = stacktrace();
-            stack.should.be.instanceOf(Array);
+describe('utils', function() {
+    describe('.stacktrace()', function() {
+        it('should return array', function() {
+            expect( stacktrace() ).to.be.an.instanceOf( Array );
         });
 
-        it('should return stack stace beginning at current function', function () {
-            function helloMyFilez () {
-                var stack = stacktrace();
-                stack[0].should.include('helloMyFilez');
+        it('should return stack stace beginning at current function', function() {
+            function helloMyFilez() {
+                var stack = stacktrace()
+                expect( stack[ 0 ] ).to.include( 'helloMyFilez' );
             }
             helloMyFilez();
         });
 
-        it('should contain function names and filepaths', function () {
-            function helloMyFilez1 () {
+        it('should contain function names and filepaths', function() {
+            function helloMyFilez1() {
                 helloMyFilez2();
             }
 
-            function helloMyFilez2 () {
+            function helloMyFilez2() {
                 var stack = stacktrace();
-                stack[0].should.include('helloMyFilez2');
-                stack[0].should.include('test.utils.stacktrace.js');
-                stack[1].should.include('helloMyFilez1');
-                stack[1].should.include('test.utils.stacktrace.js');
+                expect( stack[ 0 ] ).to.include( 'helloMyFilez2' );
+                expect( stack[ 0 ] ).to.include( 'test.utils.stacktrace.js' );
+                expect( stack[ 1 ] ).to.include( 'helloMyFilez1' );
+                expect( stack[ 1 ] ).to.include( 'test.utils.stacktrace.js' );
             }
 
             helloMyFilez1();
