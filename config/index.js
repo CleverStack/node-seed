9b588ed8e4fabbba4a2f2ab751049a312dbe98f0
Richard Gustin <richard.gustin86@gmail.com>
2014-08-29 16:47:45 +1000

    fix(config): Better handling of modelAssocations

--- config/index.js
+++ config/index.js
@@ -1,15 +1,12 @@
-var path = require( 'path' )
-  , fs = require( 'fs' )
-  , debug = require( 'debug' )( 'Config' )
-  , envOverride = process.env.NODE_ENV
-        ? process.env.NODE_ENV.toLowerCase()
-        : null
-  , fileNames = [ 'global', 'default' ]
+var path        = require( 'path' )
+  , fs          = require( 'fs' )
+  , debug       = require( 'debug' )( 'Config' )
+  , envOverride = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : null
+  , fileNames   = [ 'global', 'default' ]
   , packageJson = require( path.resolve( __dirname + '/../' ) + '/package.json' )
-  , configFiles = {
-        global: [ __dirname + '/global.json' ],
-        env: [ ]
-    };
+  , configFiles = { global: [ __dirname + '/global.json' ], env: [] }
+  , modules     = packageJson.bundledDependencies
+  , config;
 
 if ( envOverride === null ) {
     debug( 'No environment based config set using NODE_ENV, defaulting to "local" configuration' );
@@ -18,7 +15,7 @@ if ( envOverride === null ) {
 
 configFiles.env.push( [ __dirname, envOverride ].join( path.sep ) + '.json' );
 
-packageJson.bundledDependencies.forEach(function( moduleName ) {
+modules.forEach(function( moduleName ) {
     var moduleConfigPath = [ path.resolve( __dirname + '/../modules' ), moduleName, 'config', '' ].join( path.sep );
 
     fileNames.forEach(function( fileName ) {
@@ -29,4 +26,31 @@ packageJson.bundledDependencies.forEach(function( moduleName ) {
     });
 });
 
-module.exports = require( 'nconf' ).loadFilesSync( [].concat( configFiles.global, configFiles.env ) );
+config = require( 'nconf' ).loadFilesSync( [].concat( configFiles.global, configFiles.env ) );
+
+
+if ( modules.indexOf( 'clever-orm' ) !== -1 ) {
+    var modelAssociations = {};
+
+    modules.forEach( function( moduleName ) {
+        var moduleConfig = config[ moduleName ];
+        if ( typeof moduleConfig === 'object' && typeof moduleConfig.modelAssociations === 'object' ) {
+            Object.keys( moduleConfig.modelAssociations ).forEach( function( sourceModel ) {
+                modelAssociations[ sourceModel ] = modelAssociations[ sourceModel ] || {};
+                
+                var associations = moduleConfig.modelAssociations[ sourceModel ];
+                Object.keys( associations ).forEach( function( associationType ) {
+                    modelAssociations[ sourceModel ][ associationType ] = modelAssociations[ sourceModel ][ associationType ] || [];
+
+                    associations[ associationType ].forEach( function( targetModel ) {
+                        modelAssociations[ sourceModel ][ associationType ].push( targetModel );
+                    });
+                });
+            });
+        }
+    });
+
+    config[ 'clever-orm' ].modelAssociations = modelAssociations;
+}
+
+module.exports = config;
