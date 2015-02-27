8c342a1cc43b26b878800c009ed9d004e0c0f765
Richard Gustin <richard@rgnconsulting.com.au>
2015-02-28 09:16:10 +1000

    chore(refactor): Refactored Gruntfile, also expanded config prompt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,128 +11,128 @@ var path            = require( 'path' )
 process.env.NODE_PATH = helpers.nodePath();
 
 module.exports = function( grunt ) {
-    // load all grunt tasks
-    require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
+  // load all grunt tasks
+  require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
 
-    // Create the project wide
-    var gruntConfig = {
-        watch: {
-            tests: {
-                files: [ 'lib/**/*.js', 'modules/**/*.js' ],
-                tasks: [ 'mochaTest:ci' ]
-            },
-            schema: {
-                files: [
-                    'schema/seedData.json',
-                    getModulePaths( 'schema', 'seedData.json' )
-                ],
-                tasks: [ 'jsonlint', 'db' ]
-            }
-        },
-        nodemon: {
-            web: {
-                script: 'cluster.js',
-                options: {
-                    file: 'cluster.js',
-                    ignoredFiles: [ 'README.md', 'node_modules/**' ],
-                    watchedExtensions: [ 'js' ],
-                    watchedFolders: [ 'lib','modules' ],
-                    delayTime: 1,
-                    cwd: __dirname
-                }
-            }
-        },
-        mochaTest: {
-            unit: {
-                options: {
-                    require: [ 'chai' ],
-                    reporter: 'spec',
-                    timeout: 5000
-                },
-                src: [ 'tests/unit/test.utils.bootstrapEnv.js', 'tests/unit/test.utils.moduleLoader.js', 'tests/unit/test.class.Module.js', 'tests/unit/test.class.Controller.js', 'tests/unit/test.class.Service.js', 'tests/unit/*.js' ].concat( getModulePaths( 'tests', 'unit', '*.js' ) )
-            },
-            e2e: {
-                options: {
-                    require: 'chai',
-                    reporter: 'spec'
-                },
-                src: [ 'tests/integration/*.js' ].concat( getModulePaths( 'tests', 'integration', '*.js' ) )
-            },
-            ci: {
-                options: {
-                    require: 'chai',
-                    reporter: 'min'
-                },
-                src: [ 'tests/**/*.js' ].concat( getModulePaths( 'tests', 'unit', '*.js' ), getModulePaths( 'tests', 'integration', '*.js' ) )
-            }
-        },
-        concurrent: {
-            servers: {
-                tasks: [ 'watch:schema', 'nodemon:web' ],
-                options: {
-                    logConcurrentOutput: true
-                }
-            }
-        },
-        jshint: {
-            options: {
-                jshintrc:       '.jshintrc',
-                reporter:       require( 'jshint-stylish' ),
-                ignores: [
-                    './modules/**/test*/**/*.js'
-                ]
-            },
-            all: [
-                './Gruntfile.js',
-                './index.js',
-                './cluster.js',
-                './lib/**/*.js',
-                getModulePaths( '*.js' )
-            ]
-        },
-        jsonlint: {
-            all: {
-                src: [
-                    './schema/seedData.json',
-                    getModulePaths( 'schema', 'seedData.json' ),
-                    './package.json'
-                ]
-            }
+  // Create the project wide
+  var gruntConfig = {
+    watch: {
+      tests: {
+        files: [ 'lib/**/*.js', 'modules/**/*.js' ],
+        tasks: [ 'mochaTest:ci' ]
+      },
+      schema: {
+        files: [
+          'schema/seedData.json',
+          getModulePaths( 'schema', 'seedData.json' )
+        ],
+        tasks: [ 'jsonlint', 'db' ]
+      }
+    },
+    nodemon: {
+      web: {
+        script: 'cluster.js',
+        options: {
+          file: 'cluster.js',
+          ignoredFiles: [ 'README.md', 'node_modules/**' ],
+          watchedExtensions: [ 'js' ],
+          watchedFolders: [ 'config','lib','modules' ],
+          delayTime: 1,
+          cwd: __dirname
         }
-    };
-
-    // Module's Gruntfiles.js
-    var callbacks = [];
-
-    // Load all modules Gruntfiles.js
-    packageJson.bundledDependencies.forEach(function( moduleName ) {
-        var moduleGruntfile = [ path.resolve( __dirname ), 'modules', moduleName, 'Gruntfile.js' ].join( path.sep );
-        if ( fs.existsSync( moduleGruntfile ) ) {
-            var gruntfile = require( moduleGruntfile )( grunt );
-
-            // Merge (deep) the grunt config objects
-            gruntConfig = merge( gruntConfig, gruntfile[ 0 ] );
-
-            // Add the register function to our callbacks
-            callbacks.push( gruntfile[ 1 ] );
+      }
+    },
+    mochaTest: {
+      unit: {
+        options: {
+          require: [ 'chai' ],
+          reporter: 'spec',
+          timeout: 5000
+        },
+        src: [ 'tests/unit/test.utils.bootstrapEnv.js', 'tests/unit/test.utils.moduleLoader.js', 'tests/unit/test.class.Module.js', 'tests/unit/test.class.Controller.js', 'tests/unit/test.class.Service.js', 'tests/unit/*.js' ].concat( getModulePaths( 'tests', 'unit', '*.js' ) )
+      },
+      e2e: {
+        options: {
+          require: 'chai',
+          reporter: 'spec'
+        },
+        src: [ 'tests/integration/*.js' ].concat( getModulePaths( 'tests', 'integration', '*.js' ) )
+      },
+      ci: {
+        options: {
+          require: 'chai',
+          reporter: 'min'
+        },
+        src: [ 'tests/**/*.js' ].concat( getModulePaths( 'tests', 'unit', '*.js' ), getModulePaths( 'tests', 'integration', '*.js' ) )
+      }
+    },
+    concurrent: {
+      servers: {
+        tasks: [ 'watch:schema', 'nodemon:web' ],
+        options: {
+          logConcurrentOutput: true
         }
-    });
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc:       '.jshintrc',
+        reporter:       require( 'jshint-stylish' ),
+        ignores: [
+          './modules/**/test*/**/*.js'
+        ]
+      },
+      all: [
+        './Gruntfile.js',
+        './index.js',
+        './cluster.js',
+        './lib/**/*.js',
+        getModulePaths( '*.js' )
+      ]
+    },
+    jsonlint: {
+      all: {
+        src: [
+          './schema/seedData.json',
+          getModulePaths( 'schema', 'seedData.json' ),
+          './package.json'
+        ]
+      }
+    }
+  };
 
-    // Initialize the config
-    grunt.initConfig( gruntConfig );
+  // Module's Gruntfiles.js
+  var callbacks = [];
 
-    // Fire the callbacks and allow the modules to register their tasks
-    callbacks.forEach( function( cb ) {
-        cb( grunt );
-    });
+  // Load all modules Gruntfiles.js
+  packageJson.bundledDependencies.forEach(function( moduleName ) {
+    var moduleGruntfile = [ path.resolve( __dirname ), 'modules', moduleName, 'Gruntfile.js' ].join( path.sep );
+    if ( fs.existsSync( moduleGruntfile ) ) {
+      var gruntfile = require( moduleGruntfile )( grunt );
 
-    // Register all project wide tasks with grunt
-    grunt.registerTask( 'test',         [ 'mochaTest:unit', 'mochaTest:e2e', 'db' ] );
-    grunt.registerTask( 'test:unit',    [ 'mochaTest:unit', 'db' ] );
-    grunt.registerTask( 'test:e2e',     [ 'mochaTest:e2e', 'db' ] );
-    grunt.registerTask( 'test:ci',      [ 'watch:tests' ] );
-    grunt.registerTask( 'server',       [ 'concurrent:servers' ] );
-    grunt.registerTask( 'server:web',   [ 'server' ] );
-    grunt.registerTask( 'serve',        [ 'server'] );
-    grunt.registerTask( 'default',      [ 'server'] );
+      // Merge (deep) the grunt config objects
+      gruntConfig = merge( gruntConfig, gruntfile[ 0 ] );
+
+      // Add the register function to our callbacks
+      callbacks.push( gruntfile[ 1 ] );
+    }
+  });
+
+  // Initialize the config
+  grunt.initConfig( gruntConfig );
+
+  // Fire the callbacks and allow the modules to register their tasks
+  callbacks.forEach( function( cb ) {
+    cb( grunt );
+  });
+
+  // Register all project wide tasks with grunt
+  grunt.registerTask( 'test',         [ 'mochaTest:unit', 'mochaTest:e2e', 'db' ] );
+  grunt.registerTask( 'test:unit',    [ 'mochaTest:unit', 'db' ] );
+  grunt.registerTask( 'test:e2e',     [ 'mochaTest:e2e', 'db' ] );
+  grunt.registerTask( 'test:ci',      [ 'watch:tests' ] );
+  grunt.registerTask( 'server',       [ 'concurrent:servers' ] );
+  grunt.registerTask( 'server:web',   [ 'server' ] );
+  grunt.registerTask( 'serve',        [ 'server'] );
+  grunt.registerTask( 'default',      [ 'server'] );
 };
