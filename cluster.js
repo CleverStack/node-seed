f664aabfd257a84212ec519418063813699a546c
Richard Gustin <richard@rgnconsulting.com.au>
2015-03-04 07:00:51 +1000

    refactor(cluster): Made cluster.js much clearer

--- cluster.js
+++ cluster.js
@@ -1,39 +1,35 @@
-var cluster     = require( 'cluster' )
-  , debug       = require( 'debug' )( cluster.isMaster ? 'cleverstack:cluster' : 'cleverstack:server' )
-  , config      = require( './config' )
-  , os          = require( 'os' )
-  , numWorkers  = config.numChildren ? config.numChildren : os.cpus()
-  , helpers     = require( './lib/utils/helpers.js' )
-  , chalk       = require( 'chalk' );
+var os      = require('os')
+  , path    = require('path')
+  , chalk   = require('chalk')
+  , cluster = require('cluster')
+  , debug   = require('debug')(cluster.isMaster ? 'cleverstack:cluster' : 'cleverstack:app')
+  , config  = require(path.resolve(path.join(__dirname, 'config')))
+  , helpers = require(path.resolve(path.join(__dirname, 'lib', 'utils', 'helpers.js')));
 
-// Cleanup the NODE_PATH for module loading
+// Clean and set the NODE_PATH for magic modules
 process.env.NODE_PATH = helpers.nodePath();
 
-// Allow modules to hook into this file
-helpers.loadModulesFileByName( 'cluster.js', config, cluster, debug );
+debug('started with pid %s', chalk.yellow(process.pid));
+debug('%s is "%s"', chalk.magenta('NODE_PATH'), chalk.yellow(process.env.NODE_PATH));
 
-// Master process
-if ( cluster.isMaster ) {
-
-  debug( 'started with pid %s', chalk.yellow( process.pid ) );
-
-  // Output the current NODE_PATH for debugging
-  debug( 'NODE_PATH is "%s"', process.env.NODE_PATH );
-
-  // Make sure we manage child processes exiting
-  cluster.on( 'exit', function( worker, code, signal ) {
-    debug( 'Worker %s has died with code %s and signal %s - Forking new process in 2.5 seconds...', worker.pid, code, signal );
-    setTimeout( cluster.fork.bind( cluster ), 2500 );
-  });
-
-  // Create worker processes
-  for ( var i = 0; i < numWorkers; ++i ) {
-    cluster.fork();
-  }
-
+if (!cluster.isMaster) {
+  
+  // Worker Process, load app.js
+  require(path.join(__dirname, 'app.js'));
 } else {
 
-  // Load a single application worker instance
-  require( './app.js' );
+  // Master Process, setup cluster
+  debug('Loading %s hook files...', chalk.yellow('cluster.js'));
+  helpers.loadModulesFileByName('cluster.js', config, cluster, debug);
 
+  // Spawn new app process when a child dies
+  cluster.on('exit', function(worker, code, signal) {
+    debug('Worker %s has died with code %s and signal %s - Forking new process in 2.5 seconds...', worker.pid, code, signal);
+    setTimeout(cluster.fork.bind(cluster), 2500);
+  });
+
+  // Spawn app processes
+  for (var i = 0; i < config.numChildren ? config.numChildren : os.cpus(); ++i) {
+    cluster.fork();
+  }
 }
