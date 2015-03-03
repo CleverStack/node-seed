6fa8c49c4945669f9c5fb7e9555effdfc2518483
Richard Gustin <richard@rgnconsulting.com.au>
2015-03-04 07:00:41 +1000

    refactor(app): Made app.js much clearer

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
-var utils     = require('utils')
-  , parser    = require('body-parser')
-  , compress  = require('compression')
-  , override  = require('method-override')
-  , resTime   = require('response-time')
-  , logger    = require('morgan')
-  , env       = utils.bootstrapEnv()
-  , moduleLdr = env.moduleLoader
-  , cors      = require('cors')
-  , chalk     = require('chalk')
-  , debug     = require('debug')('cleverstack:server')
-  , helpers   = require('utils').helpers
-  , app       = module.exports = env.app;
+var utils        = require('utils')
+  , chalk        = require('chalk')
+  , env          = utils.bootstrapEnv()
+  , app          = env.app
+  , debug        = env.debug
+  , helpers      = utils.helpers
+  , bodyParser   = require('body-parser')
+  , moduleLoader = env.moduleLoader
 
-debug('started with pid %s', chalk.yellow(process.pid));
-
-// Allow modules to hook into this file by putting an app.js file in any modules /bin folder
-helpers.loadModulesFileByName('app.js', env.config, require('cluster'), debug);
-
-moduleLdr.on('preLoadModules', function() {
+moduleLoader.on('beforeLoad', function() {
   debug('Configuring express application...');
 
-  app.use(resTime());
-  app.use(parser.urlencoded({ extended: true }));
-  app.use(parser.json());
-  app.use(logger('dev'));
-  app.use(compress());
-  app.use(override());
-  app.use(cors(env.config.cors));
+  app.use(require('morgan')('dev'));
+  app.use(require('response-time')());
+  app.use(require('method-override')());
+  app.use(require('cors')(env.config.cors));
+  app.use(require('compression')());
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 });
 
-moduleLdr.on('modulesLoaded', function() {
+moduleLoader.on('modulesLoaded', function initializeRoutes() {
   debug('Initializing routes...');
-
-  moduleLdr.initializeRoutes();
+  moduleLoader.initializeRoutes();
 });
 
-moduleLdr.on('routesInitialized', function() {
-  debug('Setting up router and starting http server...');
+moduleLoader.on('routesInitialized', function startApp() {
+  debug('Starting http server...');
 
-  app.listen(env.webPort, function() {
-    debug('Started web server on port %s in enviromment %s', chalk.yellow(env.webPort), chalk.yellow(process.env.NODE_ENV ? process.env.NODE_ENV : "LOCAL"));
+  app.listen(env.webPort, function appReady() {
+    debug('Listening on port %s', chalk.yellow(env.webPort));
   });
 });
 
+debug('Loading %s hook files...', chalk.yellow('app.js'));
+helpers.loadModulesFileByName(__filename, env.config, require('cluster'), debug);
+
 debug('Loading modules...');
-moduleLdr.loadModules();
+moduleLoader.loadModules();
